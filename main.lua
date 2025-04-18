79f0900d13cdb6cc16d265d0891f0b4200dace1c
Rohan Badiger <rohanbadiger4738@gmail.com>
2025-04-18 20:50:23 +0530

    list the functions and define the love.load() function.

---
+++ main.lua
@@ -0,0 +1,21 @@
+--[[ 3 Functions that makes the Game Work are :-
+1. love.load() 
+-Runs when the Game starts.
+-uses the features of Love2d game engine. 
+- we are going to specify global variables, adjust the window sides
+and perform any other preliminary setup before the game starts.
+
+2. love.update(dt):- this function will be called every frame when the games running
+3. Draw()
+]] 
+
+
+
+function love.load()
+
+end
+
+
+function love.update(dt)
+
+end 
