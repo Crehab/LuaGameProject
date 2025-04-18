1da98a0d4191e621ec141168fd3eeef8f2bc826e
Rohan Badiger <rohanbadiger4738@gmail.com>
2025-04-18 20:54:46 +0530

    define love.update() function.

--- main.lua
+++ main.lua
@@ -5,7 +5,13 @@
 - we are going to specify global variables, adjust the window sides
 and perform any other preliminary setup before the game starts.
 
-2. love.update(dt):- this function will be called every frame when the games running
+2. love.update(dt)
+- holds a parameter called "dt"
+- this function updates the game loop means,
+- this function will be called every frame when the game is running
+- by default games that are created using Love2d runs on 60FPS
+- means code inside the function love.update will be running 60 times every second.
+
 3. Draw()
 ]] 
 
