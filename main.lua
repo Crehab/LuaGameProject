73e85393c3ba79aad1e21168b4db0ae008e9d56a
Rohan Badiger <rohanbadiger4738@gmail.com>
2025-04-19 15:43:45 +0530

    use setColor to color the rectangle and circle

--- main.lua
+++ main.lua
@@ -31,7 +31,9 @@ function love.update(dt)
 end 
 
 function love.draw()
+  love.graphics.setColor(1, 0, 1)
   love.graphics.rectangle("line", 315, 215, 170, 140) -- outlined rectangle demonstration.
   -- love.graphics.rectangle("fill", 0, 0, 100, 100) -- filled rectangle demonstration
+  love.graphics.setColor(1, 1, 0)
   love.graphics.circle("fill", 400, 100, 50, 50) 
 end
